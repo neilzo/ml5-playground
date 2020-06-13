bd356fc935996c831c5cd5c4980810f5b07b731a
Neil Daftary <nmd278@nyu.edu>
2020-06-13 16:58:09 -0400

    Classify soosh :sushi:

---
+++ optimizeImages.js
@@ -0,0 +1,18 @@
+const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminPngquant = require('imagemin-pngquant');
+
+(async () => {
+  const files = await imagemin(['assets/*.{jpg,png}'], {
+    destination: 'src/assets',
+    plugins: [
+      imageminJpegtran({ progressive: true }),
+      imageminPngquant({
+        quality: [0.6, 0.8]
+      })
+    ]
+  });
+
+  console.log(files);
+  console.log('DONE');
+})();
--- src/App.js
+++ src/App.js
@@ -1,63 +1,96 @@
 import React, { useEffect, useState } from 'react';
 import p5 from 'p5';
 import ml5 from 'ml5';
-import otter from './assets/otter.jpg';
+import nigiriMaguroA from '../assets/nigiri_maguro.jpg';
+import nigiriMaguroB from '../assets/nigiri_maguro_b.jpg';
+import nigiriMaguroC from '../assets/nigiri_maguro_c.jpg';
+import nigiriMaguroD from '../assets/nigiri_maguro_d.png';
+import nigiriSakeA from '../assets/nigiri_salmon.jpg';
+import nigiriSakeB from '../assets/nigiri_salmon_b.jpg';
+import nigiriSakeC from '../assets/nigiri_salmon_c.jpg';
 
 import './App.css';
 import './table.css';
-
-const temp1 = [
-  {
-    label: 'otter',
-    confidence: 0.9382053017616272,
-  },
-  {
-    label: 'weasel',
-    confidence: 0.021009963005781174,
-  },
-  {
-    label: 'black-footed ferret, ferret, Mustela nigripes',
-    confidence: 0.005373395048081875,
-  },
-];
+import './imageGrid.css';
 
 const floatToPercent = (num) => (num * 100).toFixed(4);
 
 function App() {
   let mobileNet;
-  const width = 640;
-  const height = 480;
+  let classifier;
+  let totalLoss = 0;
   const [output, setOutput] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [hasPreview, setHasPreview] = useState(false);
+  const [hasTrained, setHasTrained] = useState(false);
 
   const setup = (p) => {
     p.setup = async () => {
-      p.createCanvas(width, height);
-      p.background(0);
-      mobileNet = await ml5.imageClassifier('MobileNet');
+      mobileNet = await ml5.featureExtractor('MobileNet');
       console.log('model ready');
-      const otterImg = p.createImg(otter, 'otter', undefined, () => {
-        p.image(otterImg, 0, 0, width, height);
-      });
-      otterImg.hide();
-      mobileNet.predict(otterImg, (err, results) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        setLoading(false);
-        console.log('results', results);
-        setOutput(results);
-      });
+      classifier = await mobileNet.classification();
+      await classifier.addImage(document.getElementById('maguroA'), 'maguro');
+      await classifier.addImage(document.getElementById('maguroB'), 'maguro');
+      await classifier.addImage(document.getElementById('maguroC'), 'maguro');
+      await classifier.addImage(document.getElementById('maguroD'), 'maguro');
+      await classifier.addImage(document.getElementById('sakeA'), 'sake');
+      await classifier.addImage(document.getElementById('sakeB'), 'sake');
+      await classifier.addImage(document.getElementById('sakeC'), 'sake');
+      console.log('images added!');
+      // await train();
+      // setLoading(false);
     };
 
     p.draw = function () {};
   };
 
-  const renderOutput = () => {
-    if (loading) {
-      return <div>Loading... just chill for a sec 🏖</div>;
+  const train = async () => {
+    setLoading(true);
+    await classifier.train(whileTraining);
+    setLoading(false);
+    setHasTrained(true);
+  };
+
+  const whileTraining = (lossVal) => {
+    if (lossVal) {
+      totalLoss = lossVal;
+      console.log('loss', lossVal);
+    } else {
+      console.log('training done! final loss', totalLoss);
     }
+  };
+
+  const handleTrainClick = async () => {
+    await train();
+  };
+
+  const handlePredictClick = () => {
+    const imageToPredict = document.getElementById('preview');
+    if (!imageToPredict) {
+      throw Error('Image to guess not avail :(');
+    }
+    classifier.classify(imageToPredict, (err, result) => {
+      console.log('PREDICTION:', result);
+      setOutput(result);
+    });
+  };
+
+  const handleFileChange = (evt) => {
+    const fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      if (e.target.result) {
+        document.getElementById('preview').src = e.target.result;
+        setHasPreview(true);
+      }
+    };
+    fileReader.readAsDataURL(evt.target.files[0]);
+  };
+
+  const renderOutput = () => {
+    if (!output || !output.length) {
+      return null;
+    }
+
     return (
       <>
         <table>
@@ -81,14 +114,39 @@ function App() {
   };
 
   useEffect(() => {
-    new p5(setup, 'canvasContainer');
+    new p5(setup);
   }, []);
 
   return (
     <div className="App">
-      <div id="canvasContainer" className="col-50" />
+      <div className="datasetContainer col-50">
+        <h1>Data set</h1>
+        <button onClick={handleTrainClick} disabled={loading}>
+          {loading ? 'Training...' : 'Train'}
+        </button>
+        <div className="imageGrid">
+          <img src={nigiriMaguroA} id="maguroA" alt="" />
+          <img src={nigiriMaguroB} id="maguroB" alt="" />
+          <img src={nigiriMaguroC} id="maguroC" alt="" />
+          <img src={nigiriMaguroD} id="maguroD" alt="" />
+          <img src={nigiriSakeA} id="sakeA" alt="" />
+          <img src={nigiriSakeB} id="sakeB" alt="" />
+          <img src={nigiriSakeC} id="sakeC" alt="" />
+        </div>
+      </div>
       <div className="resultsContainer col-50">
-        <h1>Predictions:</h1>
+        <h1>Predictions</h1>
+        <div>
+          <input type="file" onChange={handleFileChange} />
+          <img id="preview" src="" alt="" className="img-preview" />
+          <button
+            onClick={handlePredictClick}
+            className="bgc-secondary"
+            disabled={!hasTrained || loading || !hasPreview}
+          >
+            Predict
+          </button>
+        </div>
         {renderOutput()}
       </div>
     </div>
